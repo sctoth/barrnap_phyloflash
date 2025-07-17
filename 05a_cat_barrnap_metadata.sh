b54119b66d52a1009860c9fa5518956d7a74aae8
Spencer Toth <sctoth@ncsu.edu>
2025-07-17 18:49:36 -0400

    edit workflow with correct directories

--- 05a_cat_barrnap_metadata.sh
+++ 05a_cat_barrnap_metadata.sh
@@ -11,10 +11,10 @@
 # ------------------------------------ #
 set -eu
 MAIN_DIR="/scratch/public/genomics/toths/biocode_fish_genome_skimming"
-SAMPLE_LIST="${MAIN_DIR}/sample_list.txt"
+SAMPLE_LIST="${MAIN_DIR}/sample_list_bacteria_all.txt"
 RRNA_GENE="16S"
 # ------------------------------------ #
-MASTER_OUT="${MAIN_DIR}/biocode_fish_barrnap/biocode_fish_barrnap_${RRNA_GENE}/filtered_${RRNA_GENE}_blast_taxonomy_no_eukaryota_with_megahit_metadata.csv"
+MASTER_OUT="${MAIN_DIR}/biocode_fish_barrnap/biocode_fish_barrnap_${RRNA_GENE}/${RRNA_GENE}_blast_taxonomy_all_with_megahit_metadata_filtered.csv"
 
 if [ ! -s "$SAMPLE_LIST" ]; then
     echo "Sample list is empty or missing!" >&2
@@ -23,12 +23,12 @@ fi
 
 # Add header (from first sample)
 FIRST_SAMPLE=$(head -n 1 "$SAMPLE_LIST")
-HEADER_FILE="${MAIN_DIR}/biocode_fish_barrnap/biocode_fish_barrnap_${RRNA_GENE}/${FIRST_SAMPLE}_metadata/${FIRST_SAMPLE}_${RRNA_GENE}_blast_taxonomy_no_eukaryota_with_megahit_metadata_filtered.csv"
+HEADER_FILE="${MAIN_DIR}/biocode_fish_barrnap/biocode_fish_barrnap_${RRNA_GENE}/${FIRST_SAMPLE}_metadata/${FIRST_SAMPLE}_${RRNA_GENE}_blast_taxonomy_all_with_megahit_metadata_filtered.csv"
 head -n 1 "$HEADER_FILE" > "$MASTER_OUT"
 
 # Concatenate all files (skip header for each)
 while read -r SAMPLE; do
-    FILE="${MAIN_DIR}/biocode_fish_barrnap/biocode_fish_barrnap_${RRNA_GENE}/${SAMPLE}_metadata/${SAMPLE}_${RRNA_GENE}_blast_taxonomy_no_eukaryota_with_megahit_metadata_filtered.csv"
+    FILE="${MAIN_DIR}/biocode_fish_barrnap/biocode_fish_barrnap_${RRNA_GENE}/${SAMPLE}_metadata/${SAMPLE}_${RRNA_GENE}_blast_taxonomy_all_with_megahit_metadata_filtered.csv"
     if [ -f "$FILE" ]; then
         echo "Merging $FILE"
         tail -n +2 "$FILE" >> "$MASTER_OUT"
